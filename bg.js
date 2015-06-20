127212ab820c4bd21c56d5d33904db0fd70e0ee9
Dez Iddon <dez@backpagecms.net>
2015-06-20 15:38:33 +0100

    Selector now passed as argument. Navigation arrows only appear when their direction is available. Addition of mobile friendly RD and swiping navigation.

--- bg.js
+++ bg.js
@@ -1,19 +1,21 @@
-;(function (win, doc, undefined) {
+;(function (selector, undefined) {
     'use strict';
     
-    var docEl = doc.documentElement,
-            currentPane = 0,
-            body = doc.body,
-            paneCount = 0,
-            nuImgs = [],
-            playTimer,
-            blogArea,
-            navRight,
-            navLeft,
-            gallery,
-            rail,
-            css,
-            UA;
+    var doc = document,
+        docEl = doc.documentElement,
+        currentPane = 0,
+        body = doc.body,
+        paneCount = 0,
+        win = window,
+        nuImgs = [],
+        playTimer,
+        blogArea,
+        navRight,
+        navLeft,
+        gallery,
+        rail,
+        css,
+        UA;
 
 
     if (!doc.querySelector || !Array.isArray || !Object.keys || docEl.className.indexOf('bpModule-_backpage') > -1) {
@@ -65,6 +67,20 @@
         };
     };
 
+    var navState = function () {
+        if (currentPane === 0) {
+            navLeft.style.display = 'none';
+        } else {
+            navLeft.style.display = 'block'
+        }
+
+        if (currentPane === (paneCount - 1)) {
+            navRight.style.display = 'none'
+        } else {
+            navRight.style.display = 'block'
+        }
+    };
+
 
     var navTo = function (paneIndex) {
         var width = offset(gallery).width;
@@ -83,17 +99,19 @@
             rail.style[UA.js + 'Transform'] = transform;
             rail.style['transform'] = transform;
         }, 9);
+
+        navState();
     };
 
 
-    blogArea = q$('.bpModule-blog-post .hfeed.post .entry')[0];
+    blogArea = q$(selector)[0];
 
     if (!blogArea) {
         return;
     }
 
     css = document.createElement('style');
-    css.innerHTML = '.smlImageGallery,.smlImageGallery .imageRail>div{height:300px;width:400px}.smlImageGallery{clear:both;overflow:hidden;position:relative;will-change:opacity}.smlImageGallery.live{opacity:1}.smlImageGallery .imageRail{height:100%;width:900px;will-change:transform,-webkit-transform}.smlImageGallery .imageRail>div{background:center center no-repeat;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover;height:100%;float:left}.galNav{background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACsAAABgCAMAAACzHHtdAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAASUExURQAAAEFAQkFAQkFAQkFAQkFAQm97d0oAAAAGdFJOUwD6H1vPmy8OSLgAAAB6SURBVHja7dU5CsBADENReZn7XzmlWxkEziROrWL4zxDQXzSmdtipmzk/DXaa9BTJP/Z0psnXSmenrVq+BjcZ8GHvMvA1WINxA77W7w3AGwCWgLpXkalPoU5Mna045Mx1PvrM6GZekwGTscwXmmBNXmayvwhB5m+axANEMANYTrUKJgAAAABJRU5ErkJggg==)center center no-repeat;cursor:pointer;height:96px;height:100%;left:0;opacity:0;padding:20px;position:absolute;width:43px;top:0;z-index:3}.smlImageGallery:hover .galNav{opacity:1}.galNav:hover{background-color:rgba(255,255,255,.7)}.galNavRight{background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACsAAABgCAMAAACzHHtdAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAVUExURQAAAEFAQkFAQkFAQkFAQkFAQkFAQjwWHqAAAAAHdFJOUwD6zxGbMmAzCf3vAAAAeElEQVR42u3UKRIAQQhDUcJ2/yOPHZkIRHeD/iqvCrM2+hJFtwUhbiCE2Pk44SbEyccuxOFoPoYQF9bkJRM/zSTW5CGTGjPpMZPU4lyTn4nym5XdBvCaSIUV9DR4iHAlzZlh2bTX4CwD3Ghga3CxQdxo4HcaWPHpB1x5A+/aEry2AAAAAElFTkSuQmCC);left:auto;right:0}.galNav,.smlImageGallery,.smlImageGallery .imageRail{-webkit-transition:.15s;-moz-transition:.15s;transition:.15s}';
+    css.innerHTML = '.smlImageGallery{clear:both;height:300px;overflow:hidden;position:relative;width:500px;will-change:opacity}.smlImageGallery.live{opacity:1}.smlImageGallery .imageRail{height:100%;width:1000%;will-change:transform,-webkit-transform;-webkit-transition:.3s;-moz-transition:.3s;transition:.3s}.smlImageGallery .imageRail>div{background:center center no-repeat;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover;height:100%;float:left;width:10%}.galNav{background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACsAAABgCAMAAACzHHtdAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAASUExURQAAAEFAQkFAQkFAQkFAQkFAQm97d0oAAAAGdFJOUwD6H1vPmy8OSLgAAAB6SURBVHja7dU5CsBADENReZn7XzmlWxkEziROrWL4zxDQXzSmdtipmzk/DXaa9BTJP/Z0psnXSmenrVq+BjcZ8GHvMvA1WINxA77W7w3AGwCWgLpXkalPoU5Mna045Mx1PvrM6GZekwGTscwXmmBNXmayvwhB5m+axANEMANYTrUKJgAAAABJRU5ErkJggg==)center center no-repeat;cursor:pointer;height:96px;height:100%;left:0;opacity:0;padding:20px;position:absolute;width:43px;top:0;z-index:3}.smlImageGallery:hover .galNav{opacity:1}.galNav:hover{background-color:rgba(255,255,255,.7)}.galNavRight{background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACsAAABgCAMAAACzHHtdAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAVUExURQAAAEFAQkFAQkFAQkFAQkFAQkFAQjwWHqAAAAAHdFJOUwD6zxGbMmAzCf3vAAAAeElEQVR42u3UKRIAQQhDUcJ2/yOPHZkIRHeD/iqvCrM2+hJFtwUhbiCE2Pk44SbEyccuxOFoPoYQF9bkJRM/zSTW5CGTGjPpMZPU4lyTn4nym5XdBvCaSIUV9DR4iHAlzZlh2bTX4CwD3Ghga3CxQdxo4HcaWPHpB1x5A+/aEry2AAAAAElFTkSuQmCC);left:auto;right:0}.galNav,.smlImageGallery{-webkit-transition:.15s;-moz-transition:.15s;transition:.15s}@media(max-width:767px){.smlImageGallery{height:500px;width:100%}.galNav:hover{background-color:transparent}}';
     doc.body.appendChild(css);
 
     gallery = doc.createElement('div');
@@ -103,9 +121,17 @@
         clearInterval(playTimer);
     };
 
+    gallery.ontouchstart = function () {
+        clearInterval(playTimer);
+    }
+
     gallery.onmouseleave = function () {
         initPlayTimer();
     };
+
+    gallery.ontouchend = function () {
+        initPlayTimer();
+    }
 
     rail = doc.createElement('div');
     rail.className = 'imageRail';
@@ -154,6 +180,7 @@
         img.src = src;
     });
 
+    // Auto play
     var play = function () {
         currentPane++;
 
@@ -167,5 +194,66 @@
         playTimer = setInterval(play, 3 * 1000);
     }
 
+    navState();
     initPlayTimer();
-}(window, document));
+
+
+    // Mobile nav
+    (function (options) {
+        var swipeRight = options.swipeRight || null,
+            swipeLeft = options.swipeLeft || null,
+            swipeDown = options.swipeDown || null,
+            swipeUp = options.swipeUp || null,
+            X = null,
+            Y = null;
+
+        var handleTouchStart = function (evt) {
+            X = evt.touches[0].clientX;
+            Y = evt.touches[0].clientY;
+        };
+
+        var handleTouchMove = function (evt) {
+            var xDiff,
+                yDiff,
+                xUp,
+                yUp;
+
+            if (!X || !Y) {
+                return;
+            }
+
+            xUp = evt.touches[0].clientX;
+            yUp = evt.touches[0].clientY;
+
+            xDiff = X - xUp;
+            yDiff = Y - yUp;
+
+            if (Math.abs(xDiff) > Math.abs(yDiff)) {
+                if (xDiff > 0) {
+                    if (swipeLeft) swipeLeft(evt);
+                } else {
+                    if (swipeRight) swipeRight(evt);
+                }
+            } else {
+                if (yDiff > 0) {
+                    if (swipeUp) swipeUp(evt);
+                } else {
+                    if (swipeDown) swipeDown(evt);
+                }
+            }
+
+            X = null;
+            Y = null;
+        };
+
+        gallery.addEventListener('touchstart', handleTouchStart, false);
+        gallery.addEventListener('touchmove', handleTouchMove, false);
+    }({
+        swipeLeft: function () {
+            navRight.click();
+        },
+        swipeRight: function () {
+            navLeft.click();
+        }
+    }));
+}('body'));
